31feee51cb4f37ceee5f3397da7ffccc84ff29f3
tk-sakaguchi <takahiro_sakaguchi@tbn.co.jp>
2022-07-18 11:01:22 +0900

    08/add:letで遅延読み込みをする

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before(:each) do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, name: 'Test Project', owner: @user)
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it 'is valid with a user, project, and message' do
-    note = FactoryBot.build(:note, project: @project)
+    note = FactoryBot.build(:note, project: project)
     expect(note).to be_valid
   end
 
@@ -21,18 +19,16 @@ RSpec.describe Note, type: :model do
 
   # 文字列に一致するメッセージを検索する
   describe 'search message for a term' do
-    before(:each) do
-      @note1 = FactoryBot.create(:note, message: 'This is the first note.', project: @project)
-      @note2 = FactoryBot.create(:note, message: 'This is the second note.', project: @project)
-      @note3 = FactoryBot.create(:note, message: 'First, preheat the oven', project: @project)
-    end
+    let!(:note1) { FactoryBot.create(:note, project: project, user: user, message: 'This is the first note') }
+    let!(:note2) { FactoryBot.create(:note, project: project, user: user, message: 'This is the second note') }
+    let!(:note3) { FactoryBot.create(:note, project: project, user: user, message: 'First, preheat the oven.') }
 
     # 一致するデータが見つかるとき
     context 'when a match is found' do
       # 検索文字列に一致するメモを返すこと
       it 'returns notes that match the search term' do
-        expect(Note.search('first')).to include(@note1, @note3)
-        expect(Note.search('second')).to include(@note2)
+        expect(Note.search('first')).to include(note1, note3)
+        expect(Note.search('second')).to include(note2)
       end
     end
 
@@ -41,6 +37,7 @@ RSpec.describe Note, type: :model do
       # 検索結果が1件も見つからなければ空のコレクションを返すこと
       it 'returns an empty collection' do
         expect(Note.search('message')).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
